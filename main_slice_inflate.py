e0ff5fc2a5c9873025340b1e0747482c38813bcf
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2023-03-08 12:17:31 +0000

    Configure stages run to only use from-dataloader targets

--- main_slice_inflate.py
+++ main_slice_inflate.py
@@ -1461,7 +1461,7 @@ elif config_dict['sweep_type'] == 'stage-sweep':
             sa_atm=get_atm(config_dict, len(training_dataset.label_tags), 'sa', THIS_SCRIPT_DIR),
             hla_atm=get_atm(config_dict, len(training_dataset.label_tags), 'hla', THIS_SCRIPT_DIR),
             cuts_mode='sa',
-            reconstruction_target='sa-oriented',
+            reconstruction_target='from-dataloader',
             epochs=40,
             soft_cut_std=-999,
             use_distance_map_localization=False,
@@ -1473,7 +1473,7 @@ elif config_dict['sweep_type'] == 'stage-sweep':
         Stage( # Optimize hla
             hla_atm=get_atm(config_dict, len(training_dataset.label_tags), 'hla', THIS_SCRIPT_DIR),
             cuts_mode='sa>hla',
-            reconstruction_target='hla-oriented',
+            reconstruction_target='from-dataloader',
             epochs=40,
             soft_cut_std=-999,
             use_distance_map_localization=False,
@@ -1485,7 +1485,7 @@ elif config_dict['sweep_type'] == 'stage-sweep':
         Stage( # Final optimized run
             do_output=True,
             cuts_mode='sa+hla',
-            reconstruction_target='hla-oriented',
+            reconstruction_target='from-dataloader',
             epochs=config_dict['epochs'],
             soft_cut_std=-999,
             use_affine_theta=True,
@@ -1496,7 +1496,7 @@ elif config_dict['sweep_type'] == 'stage-sweep':
         Stage( # Reference run
             do_output=True,
             cuts_mode='sa+hla',
-            reconstruction_target='hla-oriented',
+            reconstruction_target='from-dataloader',
             epochs=config_dict['epochs'],
             soft_cut_std=-999,
             train_affine_theta=False,
