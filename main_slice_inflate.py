Fix scaling errors when segmenting images

main_slice_inflate.py
@@ -408,8 +408,8 @@ def get_transformed(config, label, soft_label, nifti_affine, known_augment_affin
         pred_slc = eo.rearrange(pred_slc, 'B 1 D H -> B D H 1').long()
         label_slc = eo.rearrange(F.one_hot(pred_slc, num_classes),
             'B D H 1 OH -> B OH D H 1')
-        # plt.imshow(image_slc.squeeze().cpu(), cmap='gray')
-        # plt.imshow(label_slc.argmax(1).squeeze().cpu(), cmap='magma', alpha=.5, interpolation='none')
+        # plt.imshow(image_slc[0].squeeze().cpu(), cmap='gray')
+        # plt.imshow(label_slc[0].argmax(1).squeeze().cpu(), cmap='magma', alpha=.5, interpolation='none')
         # plt.savefig('slice_seg.png')
 
     # if config.crop_around_2d_label_center is not None:

slice_inflate/utils/nifti_utils.py
@@ -229,7 +229,7 @@ def nifti_grid_sample(volume:torch.Tensor, volume_affine:torch.Tensor, ras_trans
     augmented_grid_affine = (grid_affine @ pre_grid_sample_hidden_affine)
 
     grid = torch.nn.functional.affine_grid(
-        augmented_grid_affine.to(volume)[:,:3,:].view(B,3,4), target_shape, align_corners=False
+        augmented_grid_affine.to(dtype)[:,:3,:].view(B,3,4), target_shape, align_corners=False
     )
 
     if is_label:

slice_inflate/utils/nnunetv2_utils.py
@@ -91,8 +91,8 @@ def run_inference_on_image(b_image: torch.Tensor, b_spacing: torch.Tensor,
         else:
             target_spacing = nnunet_spacing
 
-        target_fov_mm = target_spacing * torch.as_tensor(img_shape).to(target_spacing)
-        target_fov_vox = torch.as_tensor(img_shape)
+        target_fov_mm = input_spacing * torch.as_tensor(img_shape).to(target_spacing)
+        target_fov_vox = (torch.as_tensor(img_shape) * input_spacing / target_spacing).int()
 
         volume_affine = torch.diag(
             torch.as_tensor(input_spacing.tolist() + [1.])
@@ -114,9 +114,10 @@ def run_inference_on_image(b_image: torch.Tensor, b_spacing: torch.Tensor,
         )
         data.append(data_elem)
 
-    seg = run_inference(data, network, parameters,
-        plans_manager, configuration_manager, dataset_json, inference_allowed_mirroring_axes,
-        device=b_image.device)
+    with suppress_stdout():
+        seg = run_inference(data, network, parameters,
+            plans_manager, configuration_manager, dataset_json, inference_allowed_mirroring_axes,
+            device=b_image.device)
 
     return torch.as_tensor(np.array(seg)).to(b_image.device)
 