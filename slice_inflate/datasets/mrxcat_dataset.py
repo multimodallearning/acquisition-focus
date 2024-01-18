1b61e78f76e24711dc0a61d0162bf0558c2d4082
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2024-01-18 14:57:20 +0000

    Add rotation augmentation

--- slice_inflate/datasets/mrxcat_dataset.py
+++ slice_inflate/datasets/mrxcat_dataset.py
@@ -122,11 +122,11 @@ class MRXCATDataset(HybridIdDataset):
             if self.do_augment:
                 sample_augment_strength = self.self_attributes['sample_augment_strength']
                 known_augment_affine = get_random_affine(
-                    rotation_strength=sample_augment_strength * .1,
+                    rotation_strength=sample_augment_strength,
                     zoom_strength=sample_augment_strength)
 
                 hidden_augment_affine = get_random_affine(
-                    rotation_strength=sample_augment_strength * .1,
+                    rotation_strength=sample_augment_strength,
                     zoom_strength=0.0)
 
             additional_data['known_augment_affine'] = known_augment_affine.view(4,4)
