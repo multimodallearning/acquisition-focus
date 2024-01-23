eac41ca4fec34015f8da4fd84ff3c4190b9183a8
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2024-01-23 17:02:10 +0000

    Fix bug in meta config runner

--- meta_configs_mmwhs_part_I/run_meta.sh
+++ meta_configs_mmwhs_part_I/run_meta.sh
@@ -1,9 +1,9 @@
 # Run main script for every meta-configuration file
 
-main_script_path=python ../main_slice_inflate.py
+main_script_path=../main_slice_inflate.py
 
 for meta_config in *.json
 do
-    echo "Running $config"
-    $main_script_path $config
+    echo "Running $meta_config"
+    python $main_script_path --meta_config_path $meta_config
 done
