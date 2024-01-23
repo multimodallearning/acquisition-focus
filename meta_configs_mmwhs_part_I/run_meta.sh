167fcfb183dbeb19c0257497c889243d88680544
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2024-01-23 17:06:49 +0000

    Improve runnig meta configs

--- meta_configs_mmwhs_part_I/run_meta.sh
+++ meta_configs_mmwhs_part_I/run_meta.sh
@@ -1,9 +1,9 @@
 # Run main script for every meta-configuration file
+cd ..
+main_script_path=./main_slice_inflate.py
 
-main_script_path=../main_slice_inflate.py
-
-for meta_config in *.json
+for meta_config in ./meta_configs_mmwhs_part_I/*.json
 do
     echo "Running $meta_config"
-    python $main_script_path --meta_config_path $meta_config
+    python $main_script_path --meta_config_path $meta_config >> ./meta_configs_mmwhs_part_I/log_all.txt
 done
