a9774151e2582072b13889547efa22bb9e8841f4
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2024-01-23 18:37:20 +0000

    Add MRXCAT meta configs

---
+++ meta_configs_MRXCAT_part_I/run_meta.sh
@@ -0,0 +1,9 @@
+# Run main script for every meta-configuration file
+cd ..
+main_script_path=./main_slice_inflate.py
+
+for meta_config in ./meta_configs_MRXCAT_part_I/*.json
+do
+    echo "Running $meta_config"
+    python $main_script_path --meta_config_path $meta_config >> ./meta_configs_MRXCAT_part_I/log_all.txt
+done
