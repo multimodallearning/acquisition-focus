7119d0f185832aa967a8afbcf69752e89f861516
Christian Weihsbach <christian.weihsbach@uni-luebeck.de>
2024-04-18 11:56:34 +0200

    Restructure meta configs

--- meta_configs/meta_configs_MMWHS/run_meta.sh
+++ meta_configs/meta_configs_MMWHS/run_meta.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR/..
+cd $SCRIPT_DIR/../..
 # Run main script for every meta-configuration file
 main_script_path=./main_slice_inflate.py
 
@@ -10,5 +10,5 @@ do
     log_file_name=$(basename $meta_config .json).log
 
     echo "Using meta-config $meta_config"
-    MY_CUDA_VISIBLE_DEVICES=1 python $main_script_path --meta_config_path $meta_config >> $SCRIPT_DIR/$log_file_name
+    CUDA_VISIBLE_DEVICES=1 python $main_script_path --meta_config_path $meta_config >> $SCRIPT_DIR/$log_file_name
 done
